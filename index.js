9b7bd299179ebf197e9acb28f5de1cf90a312f49
Knar Gocer <knargr@gmail.com>
2020-10-19 13:58:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const fi = (function () {
 
     last: function (arr, n) {
       if(n){
-        return arr.slice((arr.length)-n);
+        return arr.slice((arr.length)-n,(arr.length)-1);
       } 
       else {
         return arr[(arr.length)-1] ;
