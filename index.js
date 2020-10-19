28eff54884617138841bc36a03f3c44c36aa7866
Knar Gocer <knargr@gmail.com>
2020-10-19 14:12:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -91,9 +91,9 @@ const fi = (function () {
     sortBy: function (arr, callback) {
        let tmp = []
        tmp = arr.sort();
-       for (let i = 0; i <arr.length;i++){
-          tmp.push(((arr[i])));
-      }
+      // for (let i = 0; i <arr.length;i++){
+      //     tmp.push(((arr[i])));
+      // }
       return tmp;
     },
 
